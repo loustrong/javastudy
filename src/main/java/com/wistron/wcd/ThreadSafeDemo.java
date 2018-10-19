4f4022d17fbcfe5d7e05b2343115eede03b6ca68
loustrong <loustrong@163.com>
2018-10-19 21:57:20 +0800

    multi-thread-safe_20181019_deadlock

--- src/main/java/com/wistron/wcd/ThreadSafeDemo.java
+++ src/main/java/com/wistron/wcd/ThreadSafeDemo.java
@@ -1,4 +1,41 @@
 package com.wistron.wcd;
 
+class Ticket implements Runnable{
+
+    private int num = 100;
+    Object obj = new Object();
+    public   void run(){
+
+        while(true ) {
+            synchronized (obj) {
+                if (num > 0) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                    System.out.println(Thread.currentThread().getName() + "....sale...." + num--);
+                }
+            }
+        }
+
+    }
+}
 public class ThreadSafeDemo {
+    public static void main(String[] args) {
+        Ticket t = new Ticket();
+        Thread thread1 = new Thread(t);
+        Thread thread2 = new Thread(t);
+        Thread thread3 = new Thread(t);
+
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+
+
+    }
+
+
 }
