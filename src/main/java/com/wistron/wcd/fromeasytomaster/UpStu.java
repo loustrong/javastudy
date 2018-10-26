f218544a5cd9a8cfe0b38c6792c51b7b7b2e9d23
loustrong <loustrong@163.com>
2018-10-26 17:46:28 +0800

    Collection

--- src/main/java/com/wistron/wcd/fromeasytomaster/UpStu.java
+++ src/main/java/com/wistron/wcd/fromeasytomaster/UpStu.java
@@ -1,4 +1,24 @@
 package com.wistron.wcd.fromeasytomaster;
 
+import java.util.*;
+
 public class UpStu {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+        map.put("01","李同學");
+        map.put("02","魏同學");
+        Set <String> set = map.keySet();
+        Iterator<String> it = set.iterator();
+        System.out.println("Key集合中的元素：");
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+        Collection <String> coll = map.values();
+        it = coll.iterator();
+        System.out.println("Value集合中的元素：");
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+    }
 }
